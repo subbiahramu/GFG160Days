138b5144a67fd705077de6ee950d6a520efb5164
Subbiah-R <160407511+subbiahramu@users.noreply.github.com>
2024-11-21 21:06:24 +0530

    Add files via upload

---
+++ day5maximumprofit.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumProfit(int prices[]) {
+        // code here
+        int n=prices.length;
+        int profit=0;
+        for(int i=1;i<n;i++){
+            if(prices[i]>prices[i-1]){
+                profit+=prices[i]-prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
